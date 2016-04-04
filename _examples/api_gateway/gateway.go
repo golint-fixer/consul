4fba7e52a0420534c68061021fef3f76f2a91722
Tomas Aparicio <tomas@aparicio.me>
2016-04-04 18:54:40 +0100

    fix(lint): code style

--- _examples/api_gateway/gateway.go
+++ _examples/api_gateway/gateway.go
@@ -13,13 +13,13 @@ func main() {
 	web := consul.New(consul.NewConfig("web", "http://demo.consul.io"))
 
 	// Create the Consul client for proxy service
-	proxy := consul.New(consul.NewConfig("proxy", "http://demo.consul.io"))
+	consul := consul.New(consul.NewConfig("consul", "http://demo.consul.io"))
 
 	// Create a new vinxi proxy
 	vs := vinxi.NewServer(vinxi.ServerOptions{Port: port})
 
 	vs.Vinxi.Get("/").Use(web)
-	vs.Vinxi.Get("/proxy").Use(proxy)
+	vs.Vinxi.Get("/consul").Use(consul)
 
 	fmt.Printf("Server listening on port: %d\n", port)
 	err := vs.Listen()
